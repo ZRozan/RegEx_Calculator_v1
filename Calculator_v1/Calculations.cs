bbbade245db6662a3891c4421f6006bae6232a9f
Jairo Rozan <jairo.rosa.z@hotmail.com>
2022-08-19 21:28:28 -0300

    Initial commit

---
+++ Calculator_v1/Calculations.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace GeneralTraining
+{
+    class Calculations
+    {
+        public static void StartCalculator()
+        {
+            bool calculationsRunning;
+            double currentResult = 0;
+            Calculations calculations = new Calculations();
+
+            do
+            {
+                Console.WriteLine($"#---- Current value: {currentResult}\n");
+
+                Console.Write("Choose a calculation: +  -  *  /  'S'quared: ");
+                string calculationSwitch = Console.ReadLine().ToUpper();
+
+                switch (calculationSwitch)
+                {
+                    case "+":
+                        calculations.Sum(currentResult, out currentResult);
+                        break;
+                    case "-":
+                        calculations.Minus(currentResult, out currentResult);
+                        break;
+                    case "*":
+                        calculations.Multiply(currentResult, out currentResult);
+                        break;
+                    case "/":
+                        calculations.Division(currentResult, out currentResult);
+                        break;
+                    case "S":
+                        calculations.Squared(currentResult, out currentResult);
+                        break;
+                    default:
+                        Console.WriteLine("\n\t# Doof!! #\n~~ That is not an option, buddy.");
+                        break;
+                }
+                Console.WriteLine($"\nRESULT: {currentResult}");
+            }
+            while (calculationsRunning = AnotherCalculationQuestion());
+        }
+        public void Sum(double currentValue, out double result)
+        {
+            List<double> numbers;
+
+            numbers = numbersToCalculate();
+
+            foreach (double value in numbers)
+            {
+                currentValue += value;
+            }
+            result = currentValue;
+        }
+        public void Minus(double currentValue, out double result)
+        {
+            List<double> numbers;
+
+            double initialValue = currentValue;
+            double totalFromInput = 0;
+
+            if (initialValue == 0)
+            {
+                numbers = numbersToCalculate();
+
+                for (int i = 1; i < numbers.Count(); i++)
+                {
+                    totalFromInput += numbers[i];
+                }
+                result = numbers[0] - totalFromInput;
+            }
+            else
+            {
+                numbers = numbersToCalculate();
+
+                for (int i = 0; i < numbers.Count(); i++)
+                {
+                    totalFromInput += numbers[i];
+                }
+                result = initialValue - totalFromInput;
+            }
+        }
+        public void Multiply(double currentValue, out double result)
+        {
+            List<double> numbers;
+
+            if (currentValue == 0) { currentValue = 1; }
+            numbers = numbersToCalculate();
+            for (int i = 0; i < numbers.Count(); i++)
+            {
+                currentValue *= numbers[i];
+            }
+            result = currentValue;
+        }
+        public void Division(double currentValue, out double result)
+        {
+            double divisor;
+
+            if (currentValue == 0)
+            {
+                Console.WriteLine("\n\t#---- Choose the divisor: ----#");
+                divisor = valuesDivision();
+            }
+            else
+            {
+                divisor = currentValue;
+            }
+
+            Console.WriteLine($"\n\t#---- {divisor} Divided by: ----#");
+            double divident = valuesDivision();
+
+            if (divident == 0)
+            {
+                Console.WriteLine("You just created a BlackHole. Congrats on destroying everything.");
+                result = currentValue;
+            }
+            else
+            {
+                result = divisor / divident;
+            }
+
+            double valuesDivision()
+            {
+                double inputValue;
+
+                if (!Double.TryParse(Console.ReadLine(), out inputValue))
+                {
+                    Console.WriteLine("\n\t#---- This is not a number, you doofus! Do it again. ----#");
+                    valuesDivision();
+                }
+                return inputValue;
+            }
+        }
+        public void Squared(double currentValue, out double result)
+        {
+            if (currentValue == 0)
+            {
+                Console.WriteLine("\n\t#---- Choose a Value to calculate the square: ----#");
+                bool validNumber = Double.TryParse(Console.ReadLine(), out currentValue);
+                if (!validNumber)
+                {
+                    Console.WriteLine("\nHEY! That is illegal! ò.ó \n\tTry again.");
+                    Squared(currentValue, out result);
+                }
+            }
+            result = currentValue * currentValue;
+        }
+        static bool AnotherCalculationQuestion()
+        {
+            Console.WriteLine("\n\t#---- Continue? (Y/N) ----#");
+            string response = Console.ReadLine().ToUpper();
+
+            switch (response)
+            {
+                case "Y":
+                    Console.Clear();
+                    Console.WriteLine("\n\t#---- All right, let's go! ----#\n");
+                    return true;
+                    break;
+                case "N":
+                    return false;
+                    break;
+                default:
+                    Console.WriteLine("#-- There are only two options, c'mon... --#");
+                    return AnotherCalculationQuestion();
+                    break;
+            }
+        }
+        static List<double> numbersToCalculate()
+        {
+            List<double> numbersList = new List<double>();
+            double inputValue;
+            bool checkInputValue;
+
+            Console.WriteLine("\n\t#----    Press any letter for the result.   ----#");
+            Console.WriteLine("\t#---- Write your values separated by Enter: ----#");
+
+            do
+            {
+                checkInputValue = Double.TryParse(Console.ReadLine(), out inputValue);
+                if (checkInputValue) { numbersList.Add(inputValue); }
+            }
+            while (checkInputValue);
+
+            return numbersList;
+        }
+    }
+}
---
+++ Calculator_v1/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace GeneralTraining
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool aplicationRunning = true;
+
+            do
+            {
+                Console.WriteLine("\t************************");
+                Console.WriteLine("\t WELCOME TO THE PROGRAM");
+                Console.WriteLine("\t************************");
+                Console.WriteLine();
+
+                Console.WriteLine
+                (
+                "What do you want to do?\n\n" +
+                "\t[1] - Calculations.\n" +
+                "\t[2] - Not implemented yet.\n" +
+                "\t[3] - Also not implemented, but with option 3.\n\n" +
+                "\t[0] - Close this thing, please."
+                );
+
+                int choice;
+                bool aplicationChoice = Int32.TryParse(Console.ReadLine(), out choice);
+
+                if (!aplicationChoice)
+                {
+                    Console.Clear();
+                    Console.WriteLine("\n\tTsk, tsk.\nIf you want to close the aplication, be nice.\n");
+                }
+                else
+                {
+                    switch (choice)
+                    {
+                        case 0:
+                            Console.Clear();
+                            aplicationRunning = false;
+                            break;
+                        case 1:
+                            Console.Clear();
+                            Calculations.StartCalculator();
+                            break;
+                        case 2:
+                            Console.Clear();
+                            Console.WriteLine("\nWell, I said this was not implemented. But good try.\n");
+                            break;
+                        case 3:
+                            Console.Clear();
+                            Console.WriteLine("\nBrah... I know you can not read this, because you have not read the description, but anyway:" +
+                                "\n\tLearn to read.\n");
+                            break;
+                        default:
+                            Console.Clear();
+                            Console.WriteLine("\nNice try, smartass. Now choose a valid option.\n");
+                            break;
+                    }
+                }
+            }
+            while (aplicationRunning);
+
+            Console.WriteLine("\n\t## Press Enter to exit.");
+            Console.WriteLine();
+        }
+
+    }
+}
